cc271127141600f8822267cd3c003b44b5ed1752
Joshua Richardson <josh@cursedtale.com>
2024-09-24 21:23:00 -0700

    db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,71 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
+import { relations } from 'drizzle-orm'
 import {
-  index,
+  boolean,
+  integer,
   pgTableCreator,
+  primaryKey,
   serial,
-  timestamp,
   varchar,
-} from "drizzle-orm/pg-core";
+} from 'drizzle-orm/pg-core'
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `ofy-link-shortener_${name}`);
+export const createTable = pgTableCreator(name => `ofy-link-shortener_${name}`)
 
-export const posts = createTable(
-  "post",
+export const users = createTable('users', {
+  id: serial('id').primaryKey(),
+  clerkId: varchar('clerk_id').notNull(),
+  created: integer('created_at').default(Date.now()),
+  isActive: boolean('is_active').default(true),
+})
+
+export const links = createTable('links', {
+  id: serial('id').primaryKey(),
+  created: integer('created_at').default(Date.now()),
+  slug: varchar('slug', { length: 128 }).notNull(),
+  expires: integer('expires_at'),
+  redirectUrl: varchar('redirect_url').notNull(),
+  description: varchar('description').notNull(),
+  disabled: boolean('disabled').default(false),
+  isProtected: boolean('is_protected').default(false),
+  passwordHash: varchar('password_hash'),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id),
+})
+
+export const linkRelations = relations(links, ({ one, many }) => ({
+  user: one(users, { fields: [links.userId], references: [users.id] }),
+  linkTags: many(tagsOnLinks),
+}))
+
+export const tags = createTable('tags', {
+  id: serial('id').primaryKey(),
+  created: integer('created_at').default(Date.now()),
+  name: varchar('name', { length: 256 }).notNull(),
+  createdBy: integer('created_by')
+    .notNull()
+    .references(() => users.id),
+})
+
+export const tagRelations = relations(tags, ({ one, many }) => ({
+  user: one(users, { fields: [tags.createdBy], references: [users.id] }),
+  tagLinks: many(tagsOnLinks),
+}))
+
+export const tagsOnLinks = createTable(
+  'tags_links',
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
+    linkId: integer('link_id')
+      .notNull()
+      .references(() => links.id),
+    tagId: integer('tag_id')
+      .notNull()
+      .references(() => tags.id),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+  table => ({
+    pk: primaryKey({ columns: [table.linkId, table.tagId] }),
   })
-);
+)
+export const tagsOnLinksRelations = relations(tagsOnLinks, ({ one }) => ({
+  link: one(links, { fields: [tagsOnLinks.linkId], references: [links.id] }),
+  tag: one(tags, { fields: [tagsOnLinks.tagId], references: [tags.id] }),
+}))
