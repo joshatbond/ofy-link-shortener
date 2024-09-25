f26ae302f39f97e309020e94f88a6a53acc96639
Joshua Richardson <josh@cursedtale.com>
2024-09-24 21:38:17 -0700

    fix(dashboard): ensure dashboard is a dynamic route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 
 import { mockData } from '@/server/mockData'
 
+export const dynamic = 'force-dynamic'
+
 export default function DashboardPage() {
   return (
     <div>
