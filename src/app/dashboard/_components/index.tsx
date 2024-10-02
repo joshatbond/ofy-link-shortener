859427e03b7851d3d372b59e9feec9eeb205635c
Joshua Richardson <josh@cursedtale.com>
2024-10-01 23:22:23 -0700

    stuff

--- src/app/dashboard/_components/index.tsx
+++ src/app/dashboard/_components/index.tsx
@@ -1,55 +1,70 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Tooltip } from '@radix-ui/react-tooltip'
+import { redirect } from 'next/dist/server/api-utils'
 import Link from 'next/link'
-import { useMemo, useState } from 'react'
+import { type ReactNode, useMemo, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import {
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { type findLinks } from '@/server/db/actions'
 
-export function Dashboard(props: {
-  links: Awaited<ReturnType<typeof findLinks>>
-}) {
+export function Dashboard(props: { links: Link[] }) {
   const [activeLinkId, activeLinkIdAssign] = useState<string | null>(null)
   const activeLink = useMemo(() => {
-    if (!activeLinkId) return null
+    if (!activeLinkId) return undefined
     return props.links.find(link => link.id === activeLinkId)
   }, [activeLinkId, props.links])
 
   return (
     <div className="p-4">
       <Sheet>
-        <h1>Dashboard</h1>
-        {props.links.map(link => (
-          <div key={link.id}>
-            <div>
-              <p>{link.slug}</p>
-              <div className="flex justify-between">
-                <Link href={link.redirectUrl} target="_blank">
-                  Go to URL
-                </Link>
-
-                <SheetTrigger onClick={() => activeLinkIdAssign(link.id)}>
-                  Edit
-                </SheetTrigger>
-              </div>
-            </div>
-          </div>
-        ))}
-
         <SheetContent>
-          {activeLink ? (
-            <div>
-              <h2>{activeLink.slug}</h2>
-
-              {Object.entries(activeLink).map(([key, value]) => (
-                <div key={key} className="flex justify-between">
-                  <span>{key}</span>
-                  <span>{`${value}`}</span>
-                </div>
-              ))}
-            </div>
-          ) : null}
+          {activeLink && <EditLink link={activeLink} />}
         </SheetContent>
+
+        <h1 className="mb-8 text-xl">Dashboard</h1>
+
+        <div className="flex flex-wrap gap-4">
+          {props.links.map(link => (
+            <LinkCard key={link.id} link={link}>
+              <SheetTrigger onClick={() => activeLinkIdAssign(link.id)}>
+                Edit
+              </SheetTrigger>
+            </LinkCard>
+          ))}
+        </div>
       </Sheet>
 
       <button className="fixed bottom-4 right-4 h-12 w-12 rounded-full bg-blue-600 text-2xl">
@@ -58,3 +73,104 @@ export function Dashboard(props: {
     </div>
   )
 }
+
+function LinkCard(props: { link: Link; children?: ReactNode }) {
+  return (
+    <Card className="min-w-80">
+      <CardHeader>
+        <CardTitle>{props.link.slug}</CardTitle>
+        <CardDescription>{props.link.description}</CardDescription>
+      </CardHeader>
+
+      <CardFooter className="justify-between">
+        <Link href={props.link.redirectUrl} target="_blank">
+          Go to URL
+        </Link>
+        {props.children}
+      </CardFooter>
+    </Card>
+  )
+}
+const formSchema = z.object({
+  slug: z.string().min(1),
+  redirectUrl: z.string().url(),
+  description: z.string().optional(),
+  expires: z.coerce
+    .date()
+    .optional()
+    .refine(data => data && data > new Date(), {
+      message: 'Expiration date must be in the future',
+    })
+    .transform(data => (data ? Math.floor(data.getTime() / 1000) : null)),
+  disabled: z.boolean().nullable(),
+  isProtected: z.boolean().nullable(),
+  passwordHash: z.string().nullable(),
+})
+function EditLink(props: { link: Link }) {
+  const { id, created, userId, ...formLink } = props.link
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: formLink,
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log({ id, created, userId, ...values })
+  }
+
+  return (
+    <div>
+      <SheetTitle>{props.link.slug}</SheetTitle>
+      <SheetDescription className="sr-only">
+        Use the form to edit the link fields
+      </SheetDescription>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 py-4">
+          <FormField
+            control={form.control}
+            name="slug"
+            render={({ field }) => (
+              <FormItemWrapper
+                label="Slug"
+                description="This will be the name of the shortcut in the url"
+              >
+                <Input placeholder="pathname" {...field} />
+              </FormItemWrapper>
+            )}
+          />
+
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+function FormItemWrapper(props: {
+  children: ReactNode
+  label: string
+  description?: string
+}) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <FormItem>
+            <FormLabel>{props.label}</FormLabel>
+            <FormControl>{props.children}</FormControl>
+            <FormMessage />
+          </FormItem>
+        </TooltipTrigger>
+
+        <TooltipContent side="bottom" align="start" sideOffset={-24}>
+          {props.description && (
+            <FormDescription>{props.description}</FormDescription>
+          )}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+type Link = Awaited<ReturnType<typeof findLinks>>[number]
