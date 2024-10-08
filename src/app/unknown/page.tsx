42f94426b3df16c9b4b027cb0bc07bf4932fa550
Joshua Richardson <89271886+joshua-richardson-88@users.noreply.github.com>
2024-10-08 15:59:16 +0000

    feat(unknown-page): create a dumping ground page for bad links

---
+++ src/app/unknown/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="grid min-h-screen place-content-center">
+      <h1 className="text-center text-2xl font-semibold">
+        The page you were looking for doesn't seem to exist!
+      </h1>
+    </div>
+  )
+}
