36d5234ef1644bc0adc83e85ae5701d28708fe68
Joshua Richardson <89271886+joshua-richardson-88@users.noreply.github.com>
2024-10-08 15:59:58 +0000

    feat(slug-route): implement redirect route

--- src/app/[id]/route.ts
+++ src/app/[id]/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server'
 
-export function GET() {
-  return NextResponse.redirect('https://www.google.com')
+import { findLink } from '@/server/db/actions'
+
+export const dynamic = 'force-dynamic'
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  const link = await findLink(params.slug)
+  return NextResponse.redirect(link?.redirectUrl ?? '/unknown')
 }
