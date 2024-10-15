8bde4768dc6019e4e9c431a21a33b27df5a452c3
Joshua Richardson <josh@cursedtale.com>
2024-10-14 22:21:11 -0700

    fix(dashboard): list links from db

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,31 +1,13 @@
-import { type findLinks } from '@/server/db/actions'
+import { findLinks } from '@/server/db/actions'
 
 import { Dashboard } from './_components'
 
 export const dynamic = 'force-dynamic'
 
 export default async function DashboardPage() {
-  // const links = await findLinks({
-  //   with: { linkTags: { with: { tag: true } } },
-  // })
-  const links = Array.from({ length: 100 }).map((_, i) => createTestLink(i))
+  const links = await findLinks({
+    with: { linkTags: { with: { tag: true } } },
+  })
 
   return <Dashboard links={links} />
-}
-
-function createTestLink(
-  id: number
-): Awaited<ReturnType<typeof findLinks>>[number] {
-  return {
-    id: `${id}`,
-    slug: `test-${id}`,
-    redirectUrl: 'https://google.com',
-    description: `test-${id}`,
-    disabled: false,
-    isProtected: false,
-    passwordHash: null,
-    created: Math.floor(Date.now() / 1000),
-    expires: null,
-    userId: 1,
-  }
 }
