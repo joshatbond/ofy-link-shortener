b27de09c3010beee20bdd0e9703805377866b664
Joshua Richardson <josh@cursedtale.com>
2024-10-14 21:26:34 -0700

    fix(errors): clean up sentry-generated code

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,14 +4,12 @@ import * as Sentry from '@sentry/nextjs'
 import NextError from 'next/error'
 import { useEffect } from 'react'
 
-export default function GlobalError({
-  error,
-}: {
+export default function GlobalError(props: {
   error: Error & { digest?: string }
 }) {
   useEffect(() => {
-    Sentry.captureException(error)
-  }, [error])
+    Sentry.captureException(props.error)
+  }, [props.error])
 
   return (
     <html>
