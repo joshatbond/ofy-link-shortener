129e10be7c9854aea934776810b7624276524004
Joshua Richardson <josh@cursedtale.com>
2024-10-14 22:18:17 -0700

    fix(homepage): header isn't re-usable. move to page.tsx. Also lift the getuserdata call to the root layout, not the header because that makes way more sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { GeistSans } from 'geist/font/sans'
 import { type Metadata } from 'next'
-import Link from 'next/link'
 
 import { getUserData } from '@/auth/server/getUser'
 import { createUserFromAuth } from '@/server/db/actions'
@@ -12,27 +11,9 @@ export const metadata: Metadata = {
   icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`${GeistSans.variable} dark`}>
-      <body className="bg-neutral-950 text-neutral-50">
-        <div className="grid min-h-screen grid-rows-[auto_1fr]">
-          <Header />
-          {children}
-        </div>
-        <footer className="border-t border-t-neutral-700 p-4">
-          <p className="text-end">
-            &copy; {`${new Date().getFullYear()} J. Richardson`}
-          </p>
-        </footer>
-      </body>
-    </html>
-  )
-}
-
-async function Header() {
+export default function RootLayout(
+  props: Readonly<{ children: React.ReactNode }>
+) {
   getUserData()
     .then(user => {
       if (user) {
@@ -42,12 +23,17 @@ async function Header() {
     .catch(console.error)
 
   return (
-    <header className="bg-gradient-to-r from-neutral-950 to-gray-700/20">
-      <nav className="flex flex-wrap items-center justify-between gap-4 p-4 font-semibold">
-        <Link href="/">
-          <span className="text-2xl">ShortCuts</span>
-        </Link>
-      </nav>
-    </header>
+    <html lang="en" className={`${GeistSans.variable} dark`}>
+      <body className="bg-neutral-950 text-neutral-50">
+        <div className="grid min-h-screen grid-rows-[auto_1fr]">
+          {props.children}
+        </div>
+        <footer className="border-t border-t-neutral-700 p-4">
+          <p className="text-end">
+            &copy; {`${new Date().getFullYear()} J. Richardson`}
+          </p>
+        </footer>
+      </body>
+    </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,43 @@
+import Link from 'next/link'
+
 import { Button } from '@/components/ui/button'
 
 export default function HomePage() {
   return (
-    <main className="bg-neutral-800">
-      <div className="flex h-full flex-col items-center justify-center gap-4 bg-neutral-950 bg-gradient-to-r from-neutral-950 to-gray-700/20 px-4 py-24 md:flex-row md:gap-12 xl:gap-20">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="/logo.png"
-          alt="Logo"
-          className="hidden h-full w-full max-w-40 object-contain object-center md:block xl:max-w-60"
-        />
-        <div className="max-w-sm space-y-4">
-          <h1 className="text-3xl">Link Shortening without the hassle.</h1>
-          <p className="text-neutral-400">
-            Your time is important, skip the wait and privacy concerns with
-            ad-funded shorteners.
-          </p>
+    <>
+      <header className="bg-gradient-to-r from-neutral-950 to-gray-700/20">
+        <nav className="flex flex-wrap items-center justify-between gap-4 p-4 font-semibold">
+          <Link href="/">
+            <span className="text-2xl">ShortCuts</span>
+          </Link>
+        </nav>
+      </header>
 
-          <Button
-            asChild
-            variant="link"
-            className="w-full rounded bg-slate-700 px-3 py-2 text-lg font-semibold hover:bg-slate-600 focus-visible:bg-slate-600 active:bg-slate-600"
-          >
-            <a href="/login">Sign in to get started</a>
-          </Button>
+      <main className="bg-neutral-800">
+        <div className="flex h-full flex-col items-center justify-center gap-4 bg-neutral-950 bg-gradient-to-r from-neutral-950 to-gray-700/20 px-4 py-24 md:flex-row md:gap-12 xl:gap-20">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="hidden h-full w-full max-w-40 object-contain object-center md:block xl:max-w-60"
+          />
+          <div className="max-w-sm space-y-4">
+            <h1 className="text-3xl">Link Shortening without the hassle.</h1>
+            <p className="text-neutral-400">
+              Your time is important, skip the wait and privacy concerns with
+              ad-funded shorteners.
+            </p>
+
+            <Button
+              asChild
+              variant="link"
+              className="w-full rounded bg-slate-700 px-3 py-2 text-lg font-semibold hover:bg-slate-600 focus-visible:bg-slate-600 active:bg-slate-600"
+            >
+              <a href="/login">Sign in to get started</a>
+            </Button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
