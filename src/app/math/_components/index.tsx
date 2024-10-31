49ef9444e99e25c52d8a6724a72d11dc82ff60a0
Joshua Richardson <josh@cursedtale.com>
2024-10-31 16:34:27 +0000

    fix(math page): re-implement page using new layout. Move the copy svg code to this file

--- src/app/math/_components/index.tsx
+++ src/app/math/_components/index.tsx
@@ -4,11 +4,12 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { TooltipContent } from '@radix-ui/react-tooltip'
 import { Info, Plus, Trash2 } from 'lucide-react'
 import {
-  type ChangeEvent,
   type Dispatch,
+  Fragment,
   type ReactNode,
   type SetStateAction,
   useEffect,
+  useRef,
   useState,
 } from 'react'
 import { useForm } from 'react-hook-form'
@@ -38,6 +39,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip'
+import { useToast } from '@/hooks/use-toast'
 import { createId } from '@/lib/id'
 
 import { Chart, type Point } from './chart'
@@ -64,6 +66,8 @@ const formSchema = z.object({
 })
 
 export function ClientPage() {
+  const chartRef = useRef<SVGSVGElement>(null)
+  const { toast } = useToast()
   const formState = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     reValidateMode: 'onBlur',
@@ -86,42 +90,93 @@ export function ClientPage() {
       labelsY: '',
     },
   })
+  const [points, pointsAssign] = useState<Point[]>([])
+
+  const xLabels = formState
+    .watch('labelsX')
+    .trim()
+    .split(',')
+    .map(e => e.trim())
+    .filter(Boolean)
+  const yLabels = formState
+    .watch('labelsY')
+    .trim()
+    .split(',')
+    .map(e => e.trim())
+    .filter(Boolean)
+
+  const handleCopy = () => {
+    console.log(chartRef)
+    if (!chartRef.current) return
+
+    const svgString = new XMLSerializer().serializeToString(chartRef.current)
+
+    navigator.clipboard
+      .writeText(svgString)
+      .then(() => {
+        toast({
+          title: 'Copied!',
+          description: 'The graph was copied to your clipboard.',
+        })
+      })
+      .catch(err => {
+        toast({
+          title: 'Failed to copy!',
+          description: 'See the console for the specific error.',
+          variant: 'destructive',
+        })
+
+        console.error('Failed to copy svg', err)
+      })
+  }
 
   return (
     <div className="grid h-screen grid-rows-[5fr_minmax(300px,_3fr)]">
-      <div></div>
+      <Chart
+        width={formState.watch('width')}
+        height={formState.watch('height')}
+        precision={formState.watch('precision')}
+        xAxis={xLabels}
+        yAxis={yLabels}
+        points={formState.watch('showPoints') ? points : []}
+        useHorizontalGuidelines={formState.watch('useHorizontalGuidelines')}
+        useVerticalGuidelines={formState.watch('useVerticalGuidelines')}
+        debug={formState.watch('debug')}
+        ref={chartRef}
+      />
 
       <Form {...formState}>
         <div className="flex bg-[#121212]">
           <Tabs defaultValue="points" variant="vertical">
             <TabsList variant="vertical">
-              <TabsTrigger
-                value="points"
-                variant="vertical"
-                className="min-w-24"
+              <Button
+                onClick={handleCopy}
+                className="bg-blue-700 text-white hover:bg-blue-500 focus-visible:bg-blue-500 active:bg-blue-500"
               >
-                Plot
-              </TabsTrigger>
-
-              <TabsTrigger
-                value="settings"
-                variant="vertical"
-                className="min-w-24"
-              >
-                Settings
-              </TabsTrigger>
-
-              <div className="flex-1"></div>
-
-              <Button className="bg-blue-700 text-white hover:bg-blue-500 focus-visible:bg-blue-500 active:bg-blue-500">
                 Copy Graph
               </Button>
+
+              {
+                [
+                  { value: 'points', label: 'Plot' }, 
+                  { value: 'settings', label: 'Settings' }
+                ].map(tab => (
+                  <TabsTrigger 
+                    value={tab.value} 
+                    key={tab.value} 
+                    variant="vertical" 
+                    className="min-w-24"
+                  >
+                    {tab.label}
+                  </TabsTrigger>
+                ))
+              }
             </TabsList>
 
             <TabsContent
               value="points"
               variant="vertical"
-              className="grid grid-cols-2 gap-4 p-4"
+              className="grid grid-cols-2 gap-4 px-1 py-0"
             >
               <div className="card space-y-2 rounded p-4">
                 <h2 className="flex select-none items-center gap-1 text-xl font-semibold">
@@ -167,13 +222,37 @@ export function ClientPage() {
                   )}
                 />
               </div>
-              <div className="card space-y-8 rounded p-4"></div>
+
+              <div className="card space-y-8 rounded p-4">
+                <PointForm
+                  points={points}
+                  updatePoints={pointsAssign}
+                  xLabels={xLabels}
+                  yLabels={yLabels}
+                >
+                  <FormField
+                    control={formState.control}
+                    name="showPoints"
+                    render={({ field }) => (
+                      <FormItem className="flex items-center gap-4 space-y-0">
+                        <FormControl>
+                          <Switch
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                        <FormLabel>Show Points</FormLabel>
+                      </FormItem>
+                    )}
+                  />
+                </PointForm>
+              </div>
             </TabsContent>
 
             <TabsContent
               value="settings"
               variant="vertical"
-              className="grid grid-cols-2 gap-4 p-4"
+              className="grid grid-cols-2 gap-4 p-1 pt-0"
             >
               <div className="card space-y-4 rounded p-4">
                 <FormField
@@ -389,244 +468,146 @@ function AxesTooltip() {
   )
 }
 
-// export function ClientPage() {
-//   const [width, widthAssign] = useScopedState({ min: 0, max: 500 })
-//   const [height, heightAssign] = useScopedState({ min: 0, max: 200 })
+function PointForm(props: {
+  points: Point[]
+  xLabels: string[]
+  yLabels: string[]
+  updatePoints: Dispatch<SetStateAction<Point[]>>
+  children: ReactNode
+}) {
+  const [userPoints, userPointsAssign] = useState<
+    Array<{ id: string; x: number; y: number }>
+  >([])
+  const addNewPoint = () =>
+    userPointsAssign(p => [...p, { id: createId(), x: 0, y: 0 }])
+  const updatePoint = (point: (typeof userPoints)[number], type?: 'delete') => {
+    if (type) {
+      userPointsAssign(prev => prev.filter(p => p.id !== point.id))
+      props.updatePoints(prev => prev.filter(p => p.id !== point.id))
+      return
+    }
+    userPointsAssign(prev => prev.map(p => (p.id !== point.id ? p : point)))
+    props.updatePoints(prev => {
+      if (prev.find(p => p.id === point.id)) {
+        return prev.map(p => (p.id === point.id ? point : p))
+      } else {
+        return [...prev, point]
+      }
+    })
+  }
 
-//   const [useHorizontalGuidelines, useHorizontalGuidelinesAssign] =
-//     useState(true)
-//   const [useVerticalGuidelines, useVerticalGuidelinesAssign] = useState(true)
-//   const [xAxisEntries, xAxisEntriesAssign] = useState<string[]>([])
-//   const [xAxisInput, xAxisInputAssign] = useState(xAxisEntries.join(', '))
-//   const [yAxisEntries, yAxisEntriesAssign] = useState<string[]>([])
-//   const [yAxisInput, yAxisInputAssign] = useState(yAxisEntries.join(', '))
-//   const [points, pointsAssign] = useState<Point[]>([])
-//   const [precision, precisionAssign] = useState(2)
-//   const [debugMode, debugModeAssign] = useState(false)
-//   const [renderPoints, renderPointsAssign] = useState(true)
+  useEffect(() => {
+    function eventHandler(e: KeyboardEvent) {
+      if (e.key === 'Enter') addNewPoint()
+    }
 
-//   const handleLabelUpdate =
-//     (type: 'x' | 'y') => (e: ChangeEvent<HTMLInputElement>) => {
-//       const values = e.target.value.trim().split(',').filter(Boolean)
-//       if (type === 'x') {
-//         xAxisInputAssign(e.target.value)
-//         xAxisEntriesAssign(values)
-//       } else {
-//         yAxisInputAssign(e.target.value)
-//         yAxisEntriesAssign(values)
-//       }
-//     }
+    window.addEventListener('keydown', eventHandler)
 
-//   return (
-//     <div className="grid grid-cols-[auto_1fr] lg:grid-cols-[1fr_3fr]">
-//       <Tabs
-//         defaultValue="points"
-//         className="h-[calc(100vh-64px)] space-y-4 overflow-auto bg-[#121212] p-4"
-//       >
-//         <TabsList className="w-full">
-//           <TabsTrigger className="flex-1" value="points">
-//             Plot
-//           </TabsTrigger>
+    return () => window.removeEventListener('keydown', eventHandler)
+  }, [])
 
-//           <TabsTrigger className="flex-1" value="settings">
-//             <Settings2Icon />
-//           </TabsTrigger>
-//         </TabsList>
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h2 className="text-xl font-semibold">Points</h2>
 
-//         <TabsContent className="max-w-[250px] space-y-4" value="points">
-//           <div className="space-y-2">
-//             <h2 className="text-xl font-semibold">Axes</h2>
+        <Button variant="outline" onClick={addNewPoint}>
+          <Plus />
+        </Button>
+      </div>
 
-//             <div className="pl-8">
-//               <h3 className="text-lg font-medium">X-Axis Labels</h3>
-//               <Input
-//                 value={xAxisInput}
-//                 onChange={handleLabelUpdate('x')}
-//                 className="pl-8"
-//               />
-//             </div>
+      {props.children}
 
-//             <div className="pl-8 pt-4">
-//               <h3 className="text-lg font-medium">Y-Axis Labels</h3>
-//               <Input
-//                 value={yAxisInput}
-//                 onChange={handleLabelUpdate('y')}
-//                 className="pl-8"
-//               />
-//             </div>
-//           </div>
+      <div className="h-[159px] space-y-2 overflow-auto overscroll-contain">
+        {userPoints.map((point, index) => (
+          <PointSelect
+            key={index}
+            point={point}
+            xs={props.xLabels}
+            ys={props.yLabels}
+            updateFn={updatePoint}
+            hasFocus={index === userPoints.length - 1}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
 
-//           <PointForm
-//             points={points}
-//             updatePoints={pointsAssign}
-//             xLabels={xAxisEntries}
-//             yLabels={yAxisEntries}
-//           >
-//             <div className="flex items-center gap-4">
-//               <Switch
-//                 checked={renderPoints}
-//                 onCheckedChange={renderPointsAssign}
-//               />
-//               <div>Show Points</div>
-//             </div>
-//           </PointForm>
-//         </TabsContent>
-//       </Tabs>
+function PointSelect(props: {
+  point: { id: string; x: number; y: number }
+  xs: string[]
+  ys: string[]
+  updateFn: (
+    point: { id: string; x: number; y: number },
+    type?: 'delete'
+  ) => void
+  hasFocus: boolean
+}) {
+  const [xSelect, xAssign] = useState(
+    props.point.x ? props.xs[props.point.x] : ''
+  )
+  const [ySelect, yAssign] = useState(
+    props.point.y ? props.ys[props.point.y] : ''
+  )
 
-//       <Chart
-//         width={width.value}
-//         height={height.value}
-//         precision={precision}
-//         xAxis={xAxisEntries}
-//         yAxis={yAxisEntries}
-//         points={renderPoints ? points : []}
-//         useHorizontalGuidelines={useHorizontalGuidelines}
-//         useVerticalGuidelines={useVerticalGuidelines}
-//         debug={debugMode}
-//       />
-//     </div>
-//   )
-// }
+  const handleChange = (type: 'x' | 'y') => (value: string) => {
+    if (type === 'x') xAssign(value)
+    if (type === 'y') yAssign(value)
 
-// function PointForm(props: {
-//   points: Point[]
-//   xLabels: string[]
-//   yLabels: string[]
-//   updatePoints: Dispatch<SetStateAction<Point[]>>
-//   children: ReactNode
-// }) {
-//   const [userPoints, userPointsAssign] = useState<
-//     Array<{ id: string; x: number; y: number }>
-//   >([])
-//   const addNewPoint = () =>
-//     userPointsAssign(p => [...p, { id: createId(), x: 0, y: 0 }])
-//   const updatePoint = (point: (typeof userPoints)[number], type?: 'delete') => {
-//     if (type) {
-//       userPointsAssign(prev => prev.filter(p => p.id !== point.id))
-//       props.updatePoints(prev => prev.filter(p => p.id !== point.id))
-//       return
-//     }
-//     userPointsAssign(prev => prev.map(p => (p.id !== point.id ? p : point)))
-//     props.updatePoints(prev => {
-//       if (prev.find(p => p.id === point.id)) {
-//         return prev.map(p => (p.id === point.id ? point : p))
-//       } else {
-//         return [...prev, point]
-//       }
-//     })
-//   }
+    const x = convertValue(props.xs, type === 'x' ? value : xSelect)
 
-//   useEffect(() => {
-//     function eventHandler(e: KeyboardEvent) {
-//       if (e.key === 'Enter') addNewPoint()
-//     }
+    const y = convertValue(props.ys, type === 'y' ? value : ySelect)
 
-//     window.addEventListener('keydown', eventHandler)
+    if (x != null && y != null) props.updateFn({ id: props.point.id, x, y })
 
-//     return () => window.removeEventListener('keydown', eventHandler)
-//   }, [])
-//   return (
-//     <div className="space-y-4">
-//       <div className="flex items-center justify-between">
-//         <h2 className="text-xl font-semibold">Points</h2>
+    function convertValue(arr: string[], value?: string) {
+      if (!value) return null
+      if (value === '-2') return -2
+      return arr.indexOf(value)
+    }
+  }
+  const handleDelete = () => {
+    props.updateFn(props.point, 'delete')
+  }
 
-//         <Button variant="outline" onClick={addNewPoint}>
-//           <Plus />
-//         </Button>
-//       </div>
+  return (
+    <div className="flex gap-4">
+      <Select value={xSelect} onValueChange={handleChange('x')}>
+        <SelectTrigger autoFocus={props.hasFocus}>
+          <SelectValue placeholder="" />
+        </SelectTrigger>
 
-//       {props.children}
+        <SelectContent>
+          <SelectItem value="-2">0</SelectItem>
 
-//       <div className="space-y-2">
-//         {userPoints.map((point, index) => (
-//           <PointSelect
-//             key={index}
-//             point={point}
-//             xs={props.xLabels}
-//             ys={props.yLabels}
-//             updateFn={updatePoint}
-//             hasFocus={index === userPoints.length - 1}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
+          {props.xs.map((x, index) => (
+            <Fragment key={index}>
+              {x === '_' ? null : <SelectItem value={x}>{x}</SelectItem>}
+            </Fragment>
+          ))}
+        </SelectContent>
+      </Select>
 
-// function PointSelect(props: {
-//   point: { id: string; x: number; y: number }
-//   xs: string[]
-//   ys: string[]
-//   updateFn: (
-//     point: { id: string; x: number; y: number },
-//     type?: 'delete'
-//   ) => void
-//   hasFocus: boolean
-// }) {
-//   const [xSelect, xAssign] = useState(
-//     props.point.x ? props.xs[props.point.x] : ''
-//   )
-//   const [ySelect, yAssign] = useState(
-//     props.point.y ? props.ys[props.point.y] : ''
-//   )
+      <Select value={ySelect} onValueChange={handleChange('y')}>
+        <SelectTrigger>
+          <SelectValue placeholder="" />
+        </SelectTrigger>
 
-//   const handleChange = (type: 'x' | 'y') => (value: string) => {
-//     if (type === 'x') xAssign(value)
-//     if (type === 'y') yAssign(value)
+        <SelectContent>
+          <SelectItem value="-2">0</SelectItem>
 
-//     const x = convertValue(props.xs, type === 'x' ? value : xSelect)
+          {props.ys.map((y, index) => (
+            <Fragment key={index}>
+              {y === '_' ? null : <SelectItem value={y}>{y}</SelectItem>}
+            </Fragment>
+          ))}
+        </SelectContent>
+      </Select>
 
-//     const y = convertValue(props.ys, type === 'y' ? value : ySelect)
-
-//     if (x != null && y != null) props.updateFn({ id: props.point.id, x, y })
-
-//     function convertValue(arr: string[], value?: string) {
-//       if (!value) return null
-//       if (value === '-2') return -2
-//       return arr.indexOf(value)
-//     }
-//   }
-//   const handleDelete = () => {
-//     props.updateFn(props.point, 'delete')
-//   }
-
-//   return (
-//     <div className="flex gap-4">
-//       <Select value={xSelect} onValueChange={handleChange('x')}>
-//         <SelectTrigger autoFocus={props.hasFocus}>
-//           <SelectValue placeholder="" />
-//         </SelectTrigger>
-
-//         <SelectContent>
-//           <SelectItem value="-2">0</SelectItem>
-
-//           {props.xs.map((x, index) => (
-//             <SelectItem key={index} value={x}>
-//               {x}
-//             </SelectItem>
-//           ))}
-//         </SelectContent>
-//       </Select>
-
-//       <Select value={ySelect} onValueChange={handleChange('y')}>
-//         <SelectTrigger>
-//           <SelectValue placeholder="" />
-//         </SelectTrigger>
-
-//         <SelectContent>
-//           <SelectItem value="-2">0</SelectItem>
-
-//           {props.ys.map((y, index) => (
-//             <SelectItem key={index} value={y}>
-//               {y}
-//             </SelectItem>
-//           ))}
-//         </SelectContent>
-//       </Select>
-
-//       <Button variant="destructive" onClick={handleDelete}>
-//         <Trash2 />
-//       </Button>
-//     </div>
-//   )
-// }
+      <Button variant="destructive" onClick={handleDelete}>
+        <Trash2 />
+      </Button>
+    </div>
+  )
+}
